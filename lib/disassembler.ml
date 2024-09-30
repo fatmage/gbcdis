c3bd9172255584cee3f9b1f2414ab99578b55e9a
Michał Bednarz <mi.bed2001@gmail.com>
2024-09-30 23:29:45 +0200

    All instructions

--- lib/disassembler.ml
+++ lib/disassembler.ml
@@ -13,6 +13,266 @@ let read_2bytes ch =
   let second = read_byte ch in 
   first * 256 + second
 
+let match_prefixed file_ch =
+  match read_byte file_ch with 
+  | 0x00 -> iRLC_r8 B
+  | 0x01 -> iRLC_r8 C
+  | 0x02 -> iRLC_r8 D
+  | 0x03 -> iRLC_r8 E
+  | 0x04 -> iRLC_r8 H
+  | 0x05 -> iRLC_r8 L
+  | 0x06 -> iRLC_HLp
+  | 0x07 -> iRLC_r8 A
+  | 0x08 -> iRRC_r8 B
+  | 0x09 -> iRRC_r8 C
+  | 0x0A -> iRRC_r8 D
+  | 0x0B -> iRRC_r8 E
+  | 0x0C -> iRRC_r8 H
+  | 0x0D -> iRRC_r8 L
+  | 0x0E -> iRRC_HLp
+  | 0x0F -> iRRC_r8 A
+  | 0x10 -> iRL_r8 B
+  | 0x11 -> iRL_r8 C
+  | 0x12 -> iRL_r8 D
+  | 0x13 -> iRL_r8 E
+  | 0x14 -> iRL_r8 H
+  | 0x15 -> iRL_r8 L
+  | 0x16 -> iRL_HLp
+  | 0x17 -> iRL_r8 A
+  | 0x18 -> iRR_r8 B
+  | 0x19 -> iRR_r8 C
+  | 0x1A -> iRR_r8 D
+  | 0x1B -> iRR_r8 E
+  | 0x1C -> iRR_r8 H
+  | 0x1D -> iRR_r8 L
+  | 0x1E -> iRR_HLp
+  | 0x1F -> iRR_r8 A
+  | 0x20 -> iSLA_r8 B
+  | 0x21 -> iSLA_r8 C
+  | 0x22 -> iSLA_r8 D
+  | 0x23 -> iSLA_r8 E
+  | 0x24 -> iSLA_r8 H
+  | 0x25 -> iSLA_r8 L
+  | 0x26 -> iSLA_HLp
+  | 0x27 -> iSLA_r8 A
+  | 0x28 -> iSRA_r8 B
+  | 0x29 -> iSRA_r8 C
+  | 0x2A -> iSRA_r8 D
+  | 0x2B -> iSRA_r8 E
+  | 0x2C -> iSRA_r8 H
+  | 0x2D -> iSRA_r8 L
+  | 0x2E -> iSRA_HLp
+  | 0x2F -> iSRA_r8 A
+  | 0x30 -> iSWAP_r8 B
+  | 0x31 -> iSWAP_r8 C
+  | 0x32 -> iSWAP_r8 D
+  | 0x33 -> iSWAP_r8 E
+  | 0x34 -> iSWAP_r8 H
+  | 0x35 -> iSWAP_r8 L
+  | 0x36 -> iSWAP_HLp
+  | 0x37 -> iSWAP_r8 A
+  | 0x38 -> iSRL_r8 B
+  | 0x39 -> iSRL_r8 C
+  | 0x3A -> iSRL_r8 D
+  | 0x3B -> iSRL_r8 E
+  | 0x3C -> iSRL_r8 H
+  | 0x3D -> iSRL_r8 L
+  | 0x3E -> iSRL_HLp
+  | 0x3F -> iSRL_r8 A
+  | 0x40 -> iBIT_u3r8 0 B
+  | 0x41 -> iBIT_u3r8 0 C
+  | 0x42 -> iBIT_u3r8 0 D
+  | 0x43 -> iBIT_u3r8 0 E
+  | 0x44 -> iBIT_u3r8 0 H
+  | 0x45 -> iBIT_u3r8 0 L
+  | 0x46 -> iBIT_u3HLp 0
+  | 0x47 -> iBIT_u3r8 0 A
+  | 0x48 -> iBIT_u3r8 1 B
+  | 0x49 -> iBIT_u3r8 1 C
+  | 0x4A -> iBIT_u3r8 1 D
+  | 0x4B -> iBIT_u3r8 1 E
+  | 0x4C -> iBIT_u3r8 1 H
+  | 0x4D -> iBIT_u3r8 1 L
+  | 0x4E -> iBIT_u3HLp 1
+  | 0x4F -> iBIT_u3r8 1 A
+  | 0x50 -> iBIT_u3r8 2 B
+  | 0x51 -> iBIT_u3r8 2 C
+  | 0x52 -> iBIT_u3r8 2 D
+  | 0x53 -> iBIT_u3r8 2 E
+  | 0x54 -> iBIT_u3r8 2 H
+  | 0x55 -> iBIT_u3r8 2 L
+  | 0x56 -> iBIT_u3HLp 2
+  | 0x57 -> iBIT_u3r8 2 A
+  | 0x58 -> iBIT_u3r8 3 B
+  | 0x59 -> iBIT_u3r8 3 C
+  | 0x5A -> iBIT_u3r8 3 D
+  | 0x5B -> iBIT_u3r8 3 E
+  | 0x5C -> iBIT_u3r8 3 H
+  | 0x5D -> iBIT_u3r8 3 L
+  | 0x5E -> iBIT_u3HLp 3
+  | 0x5F -> iBIT_u3r8 3 A
+  | 0x60 -> iBIT_u3r8 4 B
+  | 0x61 -> iBIT_u3r8 4 C
+  | 0x62 -> iBIT_u3r8 4 D
+  | 0x63 -> iBIT_u3r8 4 E
+  | 0x64 -> iBIT_u3r8 4 H
+  | 0x65 -> iBIT_u3r8 4 L
+  | 0x66 -> iBIT_u3HLp 4
+  | 0x67 -> iBIT_u3r8 4 A
+  | 0x68 -> iBIT_u3r8 5 B
+  | 0x69 -> iBIT_u3r8 5 C
+  | 0x6A -> iBIT_u3r8 5 D
+  | 0x6B -> iBIT_u3r8 5 E
+  | 0x6C -> iBIT_u3r8 5 H
+  | 0x6D -> iBIT_u3r8 5 L
+  | 0x6E -> iBIT_u3HLp 5
+  | 0x6F -> iBIT_u3r8 5 A
+  | 0x70 -> iBIT_u3r8 6 B
+  | 0x71 -> iBIT_u3r8 6 C
+  | 0x72 -> iBIT_u3r8 6 D
+  | 0x73 -> iBIT_u3r8 6 E
+  | 0x74 -> iBIT_u3r8 6 H
+  | 0x75 -> iBIT_u3r8 6 L
+  | 0x76 -> iBIT_u3HLp 6
+  | 0x77 -> iBIT_u3r8 6 A
+  | 0x78 -> iBIT_u3r8 7 B
+  | 0x79 -> iBIT_u3r8 7 C
+  | 0x7A -> iBIT_u3r8 7 D
+  | 0x7B -> iBIT_u3r8 7 E
+  | 0x7C -> iBIT_u3r8 7 H
+  | 0x7D -> iBIT_u3r8 7 L
+  | 0x7E -> iBIT_u3HLp 7
+  | 0x7F -> iBIT_u3r8 7 A
+  | 0x80 -> iRES_u3r8 0 B
+  | 0x81 -> iRES_u3r8 0 C
+  | 0x82 -> iRES_u3r8 0 D
+  | 0x83 -> iRES_u3r8 0 E
+  | 0x84 -> iRES_u3r8 0 H
+  | 0x85 -> iRES_u3r8 0 L
+  | 0x86 -> iRES_u3HLp 0
+  | 0x87 -> iRES_u3r8 0 A
+  | 0x88 -> iRES_u3r8 1 B
+  | 0x89 -> iRES_u3r8 1 C
+  | 0x8A -> iRES_u3r8 1 D
+  | 0x8B -> iRES_u3r8 1 E
+  | 0x8C -> iRES_u3r8 1 H
+  | 0x8D -> iRES_u3r8 1 L
+  | 0x8E -> iRES_u3HLp 1
+  | 0x8F -> iRES_u3r8 1 A
+  | 0x90 -> iRES_u3r8 2 B
+  | 0x91 -> iRES_u3r8 2 C
+  | 0x92 -> iRES_u3r8 2 D
+  | 0x93 -> iRES_u3r8 2 E
+  | 0x94 -> iRES_u3r8 2 H
+  | 0x95 -> iRES_u3r8 2 L
+  | 0x96 -> iRES_u3HLp 2
+  | 0x97 -> iRES_u3r8 2 A
+  | 0x98 -> iRES_u3r8 3 B
+  | 0x99 -> iRES_u3r8 3 C
+  | 0x9A -> iRES_u3r8 3 D
+  | 0x9B -> iRES_u3r8 3 E
+  | 0x9C -> iRES_u3r8 3 H
+  | 0x9D -> iRES_u3r8 3 L
+  | 0x9E -> iRES_u3HLp 3
+  | 0x9F -> iRES_u3r8 3 A
+  | 0xA0 -> iRES_u3r8 4 B
+  | 0xA1 -> iRES_u3r8 4 C
+  | 0xA2 -> iRES_u3r8 4 D
+  | 0xA3 -> iRES_u3r8 4 E
+  | 0xA4 -> iRES_u3r8 4 H
+  | 0xA5 -> iRES_u3r8 4 L
+  | 0xA6 -> iRES_u3HLp 4
+  | 0xA7 -> iRES_u3r8 4 A
+  | 0xA8 -> iRES_u3r8 5 B
+  | 0xA9 -> iRES_u3r8 5 C
+  | 0xAA -> iRES_u3r8 5 D
+  | 0xAB -> iRES_u3r8 5 E
+  | 0xAC -> iRES_u3r8 5 H
+  | 0xAD -> iRES_u3r8 5 L
+  | 0xAE -> iRES_u3HLp 5
+  | 0xAF -> iRES_u3r8 5 A
+  | 0xB0 -> iRES_u3r8 6 B
+  | 0xB1 -> iRES_u3r8 6 C
+  | 0xB2 -> iRES_u3r8 6 D
+  | 0xB3 -> iRES_u3r8 6 E
+  | 0xB4 -> iRES_u3r8 6 H
+  | 0xB5 -> iRES_u3r8 6 L
+  | 0xB6 -> iRES_u3HLp 6
+  | 0xB7 -> iRES_u3r8 6 A
+  | 0xB8 -> iRES_u3r8 7 B
+  | 0xB9 -> iRES_u3r8 7 C
+  | 0xBA -> iRES_u3r8 7 D
+  | 0xBB -> iRES_u3r8 7 E
+  | 0xBC -> iRES_u3r8 7 H
+  | 0xBD -> iRES_u3r8 7 L
+  | 0xBE -> iRES_u3HLp 7
+  | 0xBF -> iRES_u3r8 7 A
+  | 0xC0 -> iSET_u3r8 0 B
+  | 0xC1 -> iSET_u3r8 0 C
+  | 0xC2 -> iSET_u3r8 0 D
+  | 0xC3 -> iSET_u3r8 0 E
+  | 0xC4 -> iSET_u3r8 0 H
+  | 0xC5 -> iSET_u3r8 0 L
+  | 0xC6 -> iSET_u3HLp 0
+  | 0xC7 -> iSET_u3r8 0 A
+  | 0xC8 -> iSET_u3r8 1 B
+  | 0xC9 -> iSET_u3r8 1 C
+  | 0xCA -> iSET_u3r8 1 D
+  | 0xCB -> iSET_u3r8 1 E
+  | 0xCC -> iSET_u3r8 1 H
+  | 0xCD -> iSET_u3r8 1 L
+  | 0xCE -> iSET_u3HLp 1
+  | 0xCF -> iSET_u3r8 1 A
+  | 0xD0 -> iSET_u3r8 2 B
+  | 0xD1 -> iSET_u3r8 2 C
+  | 0xD2 -> iSET_u3r8 2 D
+  | 0xD3 -> iSET_u3r8 2 E
+  | 0xD4 -> iSET_u3r8 2 H
+  | 0xD5 -> iSET_u3r8 2 L
+  | 0xD6 -> iSET_u3HLp 2
+  | 0xD7 -> iSET_u3r8 2 A
+  | 0xD8 -> iSET_u3r8 3 B
+  | 0xD9 -> iSET_u3r8 3 C
+  | 0xDA -> iSET_u3r8 3 D
+  | 0xDB -> iSET_u3r8 3 E
+  | 0xDC -> iSET_u3r8 3 H
+  | 0xDD -> iSET_u3r8 3 L
+  | 0xDE -> iSET_u3HLp 3
+  | 0xDF -> iSET_u3r8 3 A
+  | 0xE0 -> iSET_u3r8 4 B
+  | 0xE1 -> iSET_u3r8 4 C
+  | 0xE2 -> iSET_u3r8 4 D
+  | 0xE3 -> iSET_u3r8 4 E
+  | 0xE4 -> iSET_u3r8 4 H
+  | 0xE5 -> iSET_u3r8 4 L
+  | 0xE6 -> iSET_u3HLp 4
+  | 0xE7 -> iSET_u3r8 4 A
+  | 0xE8 -> iSET_u3r8 5 B
+  | 0xE9 -> iSET_u3r8 5 C
+  | 0xEA -> iSET_u3r8 5 D
+  | 0xEB -> iSET_u3r8 5 E
+  | 0xEC -> iSET_u3r8 5 H
+  | 0xED -> iSET_u3r8 5 L
+  | 0xEE -> iSET_u3HLp 5
+  | 0xEF -> iSET_u3r8 5 A
+  | 0xF0 -> iSET_u3r8 6 B
+  | 0xF1 -> iSET_u3r8 6 C
+  | 0xF2 -> iSET_u3r8 6 D
+  | 0xF3 -> iSET_u3r8 6 E
+  | 0xF4 -> iSET_u3r8 6 H
+  | 0xF5 -> iSET_u3r8 6 L
+  | 0xF6 -> iSET_u3HLp 6
+  | 0xF7 -> iSET_u3r8 6 A
+  | 0xF8 -> iSET_u3r8 7 B
+  | 0xF9 -> iSET_u3r8 7 C
+  | 0xFA -> iSET_u3r8 7 D
+  | 0xFB -> iSET_u3r8 7 E
+  | 0xFC -> iSET_u3r8 7 H
+  | 0xFD -> iSET_u3r8 7 L
+  | 0xFE -> iSET_u3HLp 7
+  | 0xFF -> iSET_u3r8 7 A
+  | _ -> iNOP
+
 let decode_file = fun file_ch ->
   let rec decode_aux = fun acc ->
     match In_channel.input_byte file_ch with 
@@ -276,6 +536,8 @@ let decode_file = fun file_ch ->
         (* | 0xFD -> iNOP  *)
         | 0xFE -> let n8 = read_byte file_ch in iCP_An8 n8
         | 0xFF -> iRST_vec 0x38
+        (* CB-prefixed *)
+        | 0xCB -> match_prefixed file_ch
         | _    -> iNOP 
       in decode_aux (next_instruction :: acc)
     in decode_aux []
